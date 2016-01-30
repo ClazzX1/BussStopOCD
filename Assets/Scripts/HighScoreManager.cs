c8018028e16bcc1fde739ce857c578f182b44598
avoppilas <avoppilas@htkmtav-41407.local>
2016-01-30 17:36:25 +0200

    Stuff

---
+++ Assets/Scripts/HighScoreManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HighScoreManager : MonoBehaviour {
+
+	public static bool levelComplete;
+	public string highscorePos;
+	public static int score;
+	public static int temp;
+
+	public Text scoreText;
+
+	void Start () 
+	{
+		score = 0;
+		OnGameComplete ();
+	}
+
+	public static void OnGameComplete () {
+		levelComplete = true;
+		//score = 10000; //values from your scoring logic
+		for (int i = 1; i <= 5; i++) //for top 5 highscores
+		{
+			if (PlayerPrefs.GetInt ("highscorePos" + i) < score)   //if cuurent score is in top 5
+			{
+				temp = PlayerPrefs.GetInt ("highscorePos" + i);    //store the old highscore in temp varible to shift it down 
+				PlayerPrefs.SetInt ("highscorePos" + i, score);    //store the currentscore to highscores
+				if (i < 5)                                         //do this for shifting scores down
+				{
+					int j = i + 1;
+					score = PlayerPrefs.GetInt ("highscorePos" + j);
+					PlayerPrefs.SetInt ("highscorePos" + j ,temp);    
+				}
+			}
+		}
+	}
+
+	void ScoreBoard () {
+		if (levelComplete)
+		{
+			for (int y = 1; y <= 5; y++)
+			{
+				scoreText.text = y + ". " + PlayerPrefs.GetInt("highscorePos" + y);
+				Instantiate (scoreText, new Vector2 (415, 420 - (40 * y)), Quaternion.identity);
+			}
+			levelComplete = false;
+		}
+	}
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+
+
+	static ScoreManager instance = null;
+
+	public static int score = 0;
+	int [] highScore = new int[5];
+	public Text highScoreText;
+
+	void Awake () {
+
+		if (instance != null && instance != this) {
+			Destroy (gameObject);
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+
+	}
+
+	void Start () {
+
+		highScore [0] = 1000000;
+		highScore [1] = 500000;
+		highScore [2] = 250000;
+		highScore [3] = 50000;
+		highScore [4] = 1000;
+
+		UpdateHighScoreText ();
+
+	}
+
+	void Update () {
+	
+		UpdateHighScore ();
+
+	}
+
+	void UpdateHighScore () {
+
+		if (score > highScore[0]) {
+			highScore[0] = score;
+		} else if (score < highScore[0] && score > highScore[1]) {
+			highScore[1] = score;
+		} else if (score < highScore[1] && score > highScore[2]) {
+			highScore[2] = score;
+		} else if (score < highScore[2] && score > highScore[3]) {
+			highScore[3] = score;
+		} else if (score < highScore[3] && score > highScore[4]) {
+			highScore[4] = score;
+		}
+	}
+
+	void UpdateHighScoreText () {
+		for (int i = 0; i < 5; i++) {
+			highScoreText.text = i + 1 + ". " + highScore [i].ToString ();
+			Instantiate (highScoreText, new Vector2 (415, 420 - (40 * (i + 1))), Quaternion.identity);
+		}
+	}
+}
